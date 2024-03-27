a4334dcfaa4c60d93279f41c21198c5a2dafbcfe
Ryan Montville <ryan.a.montville@gmail.com>
2024-03-27 15:01:19 -0500

    adding screenshot

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ from generator import Generator
 screen = Screen()
 screen.setup(width=800,height=600)
 screen.title("Maze Generator")
-screen.tracer(1,0)
+screen.tracer(25,0)
 generator = Generator()
+generator.hideturtle()
 generator.draw_border()
 
 generator.draw_all_vertical()
